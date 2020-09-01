a16053a54f673f1efe76e9f7858af7a51e2bca1a
Andrea Goodson <akgdev@icloud.com>
2020-09-01 22:18:47 +0100

    Adding new rates

--- js/data.js
+++ js/data.js
@@ -1,207 +1,277 @@
 var exchangeRates = [
   {
+    date: '2020-08-01',
+    rate: 1.1578,
+    denomination: 'USD',
+  },
+  {
+    date: '2020-07-01',
+    rate: 1.1232,
+    denomination: 'USD',
+  },
+  {
+    date: '2020-06-01',
+    rate: 1.0958,
+    denomination: 'USD',
+  },
+  {
+    date: '2020-05-01',
+    rate: 1.0867,
+    denomination: 'USD',
+  },
+  {
+    date: '2020-03-01',
+    rate: 1.08,
+    denomination: 'USD',
+  },
+  {
+    date: '2020-02-01',
+    rate: 1.1088,
+    denomination: 'USD',
+  },
+  {
+    date: '2020-01-01',
+    rate: 1.1115,
+    denomination: 'USD',
+  },
+  {
     date: '2019-01-01',
     rate: 1.1405,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2019-02-01',
     rate: 1.1367,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2019-03-01',
     rate: 1.1342,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2019-04-01',
     rate: 1.1354,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2019-05-01',
     rate: 1.1301,
-    denomination: 'USD'
+    denomination: 'USD',
+  },
+  {
+    date: '2019-06-01',
+    rate: 1.1171,
+    denomination: 'USD',
+  },
+  {
+    date: '2019-07-01',
+    rate: 1.1207,
+    denomination: 'USD',
+  },
+  {
+    date: '2019-08-01',
+    rate: 1.114,
+    denomination: 'USD',
+  },
+  {
+    date: '2019-09-01',
+    rate: 1.1104,
+    denomination: 'USD',
+  },
+  {
+    date: '2019-10-01',
+    rate: 1.1053,
+    denomination: 'USD',
+  },
+  {
+    date: '2019-11-01',
+    rate: 1.1123,
+    denomination: 'USD',
+  },
+  {
+    date: '2019-12-01',
+    rate: 1.1059,
+    denomination: 'USD',
   },
   {
     date: '2018-01-01',
     rate: 1.1845,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2018-02-01',
     rate: 1.2352,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2018-03-01',
     rate: 1.2312,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2018-04-01',
     rate: 1.2286,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2018-05-01',
     rate: 1.2388,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2018-06-01',
     rate: 1.1708,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2018-07-01',
     rate: 1.1578,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2018-08-01',
     rate: 1.1611,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2018-09-01',
     rate: 1.1616,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2018-10-01',
     rate: 1.1667,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2018-11-01',
     rate: 1.1389,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2018-12-01',
     rate: 1.1409,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2017-01-01',
     rate: 1.0421,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2017-02-01',
     rate: 1.0664,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2017-03-01',
     rate: 1.0555,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2017-04-01',
     rate: 1.0807,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2017-05-01',
     rate: 1.0725,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2017-06-01',
     rate: 1.1193,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2017-07-01',
     rate: 1.1147,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2017-08-01',
     rate: 1.1533,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2017-09-01',
     rate: 1.1799,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2017-10-01',
     rate: 1.2007,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2017-11-01',
     rate: 1.1749,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2017-12-01',
     rate: 1.1749,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2016-01-01',
     rate: 1.0952,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2016-02-01',
     rate: 1.0907,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2016-03-01',
     rate: 1.1136,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2016-04-01',
     rate: 1.1171,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2016-05-01',
     rate: 1.1379,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2016-06-01',
     rate: 1.1279,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2016-07-01',
     rate: 1.1283,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2016-08-01',
     rate: 1.1013,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2016-09-01',
     rate: 1.1268,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2016-10-01',
     rate: 1.115,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2016-11-01',
     rate: 1.0979,
-    denomination: 'USD'
+    denomination: 'USD',
   },
   {
     date: '2016-12-01',
     rate: 1.0602,
-    denomination: 'USD'
-  }
+    denomination: 'USD',
+  },
 ];
